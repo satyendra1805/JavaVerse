20bacdf21f477a5e90b325e3c665a34c7401a2bd
Satyendra Singh Rathore <satyendra.rathore1805@gmail.com>
2025-01-30 22:20:17 +0530

    Add files via upload

---
+++ PalindromeChecker.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static boolean isPalindrome(String str) {
+        
+        String cleanedStr = str.replaceAll("[\\W]", "").toLowerCase();
+        
+        int length = cleanedStr.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (cleanedStr.charAt(i) != cleanedStr.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a word or phrase:");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+        
+        scanner.close();
+    }
+}
